347666c997ea3305ccb3b4dba3f4679daf7ed067
Bobby Vasquez <shootbv@gmail.com>
2021-07-21 09:54:28 -0600

    Add basic styling to cards to more closely resemble provided comp

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from "react"
+import './Card.css'
 
 const Card = ({id, name, number, date, time}) => {
   return (
     <div className="card">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <p>{date}</p>
-      <p>{time}</p>
+      <p>{time} pm</p>
       <p>Number of guests: {number}</p>
     </div>
   )
